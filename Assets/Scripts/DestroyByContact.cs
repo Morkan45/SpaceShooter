bedb15804bb38e75c0c93cd94607c9d3f4183ceb
Morkansdator\Morkan <markust.dahl@gmail.com>
2017-03-24 22:56:25 +0100

    First big commit of Assets and ProjectSettings

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    public int scoreValue;
+    private GameController gameController;
+
+    public int asteroidHP;
+    private int hitCount = 0;
+
+   void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if(gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log("Cannot find 'GameController' Script");
+        }
+    } 
+
+    void countHit()
+    {
+        hitCount++;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Boundary")
+        {
+            return;
+        }
+
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+        }
+        Destroy(other.gameObject);
+        countHit();
+        if(hitCount > asteroidHP)
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+
+
+            Destroy(gameObject);
+            gameController.AddScore(scoreValue);
+        }
+
+    }
+
+}
